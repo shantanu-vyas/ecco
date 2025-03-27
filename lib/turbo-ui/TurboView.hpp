2f0efe7dd096c00d4ea925ad4e235b08fd6df135
shantanu <shantanu.vyas@smith-nephew.com>
2025-03-27 01:16:52 -0400

    adding more to view

--- lib/turbo-ui/TurboView.hpp
+++ lib/turbo-ui/TurboView.hpp
@@ -2,63 +2,73 @@
 #define TURBOVIEW_HPP
 
 #import "../ecco/EccoObject.hpp"
+#include <glm/glm.hpp>
 #include <memory>
 
 namespace ecco {
 namespace Turbo {
 
-struct Rect
-{
-    float m_width, m_height, m_x, m_y;
+struct Rect {
+  Rect(const glm::dvec2 &center, const glm::dvec2 &size)
+      : m_center(center), m_size(size) {};
+  Rect(float x, float y, float width, float height)
+      : m_center(x, y), m_size(width, height) {};
+
+  glm::dvec2 m_center;
+  glm::dvec2 m_size;
 };
 
-public class TurboView :
-            public EccoObject,
-            public std::enable_shared_from_this<TurboView>
-{
-    public:
-        virtual TurboView() = 0;
-        ~TurboView() = default;
-        virtual void Initialize();
-        virtual void DeInitialize();
 
-        //how can i variant this to be either a (float,float,float,float), Rect
-        void SetFrame(std::variant<std::tuple<float,float,float,float>, const Rect&>);
-        void SetWidth(float w);
-        void SetHeight(float h);
-        void SetX(float x);
-        void SetY(float y);
+class TurboView : public EccoObject,
+                  public std::enable_shared_from_this<TurboView> {
+public:
+   TurboView(std::string name) = delete;
+  ~TurboView() = default;
+  virtual void Initialize() = 0;
+  virtual void DeInitialize() = 0;
 
-        void SetMargin();
-        void SetTopMargin();
-        void SetBottomMargin();
-        void SetLeftMargin();
-        void SetRightMargin();
+  // how can i variant this to be either a (float,float,float,float), Rect
+  void SetFrame(const Rect &rect);
+  void SetCenter(const glm::dvec2 &center);
+  void SetX(float x);
+  void SetY(float y);
+  void SetSize(const glm::dvec2 &size);
+  void SetWidth(float w);
+  void SetHeight(float h);
 
-        void SetPadding();
-        void SetTopPadding();
-        void SetBottomPadding();
-        void SetLeftPadding();
-        void SetRightPadding();
+  // TODO margins and padding
 
-        virtual void PrePreRender() = 0;  //Runs before any of the objects [Prerender, Render, Postrender] cycle
-        virtual void PreRender() = 0;
-        virtual void Render();
-        virtual void PostRender() = 0;
-        virtual void PostPostRender() = 0; //Runs after all objects [Prerender, Render, Postrender] cyclex
+  virtual void PrePreRender() = 0; // Runs before any of the objects [Prerender, Render, Postrender] cycle
+  virtual void PreRender() = 0;
+  virtual void Render();
+  virtual void PostRender() = 0;
+  virtual void PostPostRender() = 0; // Runs after all objects [Prerender,
+                                     // Render, Postrender] cyclex
+
+  void AddChild(const std::shared_ptr<TurboView> child);
+  [[nodiscard]] bool HasParent();
+  [[nodiscard]] std::weak_ptr<TurboView> GetParent();
+  virtual bool IsRoot();
+  void SetOnResizeCBFunction(const std::function<void(void)>& func);
+
+private:
+  Rect m_bounds;
+  Rect m_margins;
+  Rect m_padding;
+  std::weak_ptr<TurboView> m_parent;
+  std::vector<std::shared_ptr<TurboView>> m_children;
+  float m_frameTime; // this shold be a chrono type
+
+  std::function<void(void)> m_onResizeCB;
+};
+
+class RootTurboView : public TurboView {
+  virtual bool IsRoot() override { return true; };
+  std::weak_ptr<TurboView> GetParent() = delete;
+};
 
 
-        private:
-        Rect m_bounds;
-        Rect m_margins;
-        Rect m_padding;
-        std::weak_ptr<TurboView> m_parent;
-        std::vector<std::shared_ptr<TurboView>> m_children;
-        std::float m_frameTime; //this shold be a chrono type
-
-}
-
-}
+} // namespace Turbo
 } // namespace ecco
 
 #endif
